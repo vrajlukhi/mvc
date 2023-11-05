a35d8d9838ecebd8430abdea025d0624ad3f1d07
vrajlukhi <130537393+vrajlukhi@users.noreply.github.com>
2023-11-05 22:50:29 +0530

    cookie

--- middlewears/auth.js
+++ middlewears/auth.js
@@ -1,11 +1,24 @@
 const auth=(req,res,next)=>{
     if(req.user){
-        next(
-
-        )
+        next()
     }
     else{
         res.redirect("/login")
     }
 }
-module.exports={auth}
+const finduser = async(req ,res, next) =>{
+    let {id} = req.cookies
+    let data = await users.findOne()
+    if(id){
+        if(data.name == "piyush"){
+            next()
+        }
+        else{
+            res.send({message : "you are not access this site"})
+        }
+    }
+    else{
+        res.redirect("/login")
+    }
+}
+module.exports={auth,finduser}
